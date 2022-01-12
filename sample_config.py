ab7c3a59a176e11f4fb031e386f80f3bdccbc36b
Soebb <87156166+Soebb@users.noreply.github.com>
2022-01-13 02:36:46 +0330

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("API_ID", ""))
 
     # Get from my.telegram.org
     API_HASH = os.environ.get("API_HASH", "")
